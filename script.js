54c806083a851bce3a0a5898398e2b8b99880e25
Conejo de la Siniestra <martosmoralluisalberto@gmail.com>
2024-08-22 18:13:04 +0200

    Programación base del JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const display = document.querySelector('display');
+const display = document.querySelector('#display');
 
 const buttons = document.querySelectorAll('button');
 
@@ -8,7 +8,14 @@ buttons.forEach((item)=>{
             display.innerText = "";
         }else if(item.id == "backspace"){
             let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length-1);
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != "" && item.id=="equal"){
+            display.innerText = eval (display.innerText);
+        } else if(display.innerText == "" && item.id== "equal"){
+            display.innerText = "Null";
+            setTimeout(() => (display.innerText = ""), 2000);
+        } else{
+            display.innerText += item.id;
         }
     }
 });
